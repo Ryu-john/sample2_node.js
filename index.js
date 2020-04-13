190c481379965e6c68bdf543db0fd766fbe4df54
john <ryu.surface@gmail.com>
2020-04-13 18:22:54 +0900

    ejs

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require('express')
 const path = require('path')
+
 const app = new express()
+const ejs = require('ejs')
+app.set('view engine','ejs')
 app.use(express.static('public'))
 
 app.listen(4000, ()=>{
   console.log('App listening on port 4000')
 })
 app.get('/',(req,res)=>{
-  res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+  // res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+  res.render('index')
 })
 app.get('/about',(req,res)=>{
-  res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+  // res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+  res.render('about')
 })
 app.get('/contact',(req,res)=>{
-  res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+  // res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+  res.render('contact')
 })
 app.get('/post',(req,res)=>{
-  res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+  // res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+  res.render('post')
 })
