ce8d9b4621281c78ff08faa8dc437a8998df8461
john <ryu.surface@gmail.com>
2020-04-15 22:15:10 +0900

    chapter6 finnished

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser')
 
 const BlogPost = require('./models/BlogPost')
 
+
 app.set('view engine','ejs')
 app.use(express.static('public'))
 mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true})
@@ -18,24 +19,33 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.listen(4000, ()=>{
   console.log('App listening on port 4000')
 })
+
 app.get('/',async (req,res)=>{
   const blogposts = await BlogPost.find({})
   res.render('index',{
     blogposts
   })
 })
+
 app.get('/about',(req,res)=>{
   res.render('about')
 })
+
 app.get('/contact',(req,res)=>{
   res.render('contact')
 })
-app.get('/post',(req,res)=>{
-  res.render('post')
+
+app.get('/post/:id',async (req,res)=>{
+  const blogpost = await BlogPost.findById(req.params.id)
+  res.render('post',{
+    blogpost
+  })
 })
+
 app.get('/posts/new',(req,res)=>{
   res.render('create')
 })
+
 app.post('/posts/store', async (req,res)=>{
   await BlogPost.create(req.body)
   res.redirect('/')
--- models/BlogPost.js
+++ models/BlogPost.js
@@ -3,7 +3,12 @@ const Schema = mongoose.Schema
 
 const BlogPostSchema = new Schema({
   title: String,
-  body: String
+  body: String,
+  username: String,
+  datePosted:{ /* can declare property type with an object like this because wi need 'default' */
+    type: Date,
+    default: new Date()
+  }
 })
 
 const BlogPost = mongoose.model('BlogPost',BlogPostSchema)
