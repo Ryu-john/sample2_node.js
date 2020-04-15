4cfbe7ee3c96465f9e141406e0fc8a512b90715c
john <ryu.surface@gmail.com>
2020-04-15 21:35:22 +0900

    display blogposts

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
 const express = require('express')
-// const path = require('path')
 const mongoose = require('mongoose')
 
 const app = new express()
 const ejs = require('ejs')
+
+const bodyParser = require('body-parser')
+
+const BlogPost = require('./models/BlogPost')
+
 app.set('view engine','ejs')
 app.use(express.static('public'))
-mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser:true})
+mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.listen(4000, ()=>{
   console.log('App listening on port 4000')
 })
-app.get('/',(req,res)=>{
-  // res.sendFile(path.resolve(__dirname, 'pages/index.html'))
-  res.render('index')
+app.get('/',async (req,res)=>{
+  const blogposts = await BlogPost.find({})
+  res.render('index',{
+    blogposts
+  })
 })
 app.get('/about',(req,res)=>{
-  // res.sendFile(path.resolve(__dirname, 'pages/about.html'))
   res.render('about')
 })
 app.get('/contact',(req,res)=>{
-  // res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
   res.render('contact')
 })
 app.get('/post',(req,res)=>{
-  // res.sendFile(path.resolve(__dirname, 'pages/post.html'))
   res.render('post')
 })
+app.get('/posts/new',(req,res)=>{
+  res.render('create')
+})
+app.post('/posts/store', async (req,res)=>{
+  await BlogPost.create(req.body)
+  res.redirect('/')
+})
+
