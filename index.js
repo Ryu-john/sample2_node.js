3cf3e544022f35b2aa62c0b2994dee5f43cc81d7
john <ryu.surface@gmail.com>
2020-04-20 16:17:16 +0900

    chapter10 finnished

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ const storePostController = require('./controllers/storePost.js')
 const getPostController = require('./controllers/getPost.js')
 const newPostController = require('./controllers/newPost.js')
 
+const newUserController = require('./controllers/newUser')
+const storeUserController = require('./controllers/storeUser')
+
+const loginController = require('./controllers/login')
+const loginUserController = require('./controllers/loginUser')
+
 // const validateMiddleware = require("./middleware/validationMiddleware")
 // app.use('/posts/store',validateMiddleware)
 
@@ -32,4 +38,10 @@ app.get('/',homeController)
 app.get('/post/:id',getPostController)
 app.post('/posts/store',storePostController)
 app.get('/posts/new',newPostController)
+
+app.get('/auth/register', newUserController)
+app.post('/users/register', storeUserController)
+
+app.get('/auth/login', loginController)
+app.post('/users/login',loginUserController)
 
