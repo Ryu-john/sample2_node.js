f977c1262a9deda633d118744996484bc3fce0ac
john <ryu.surface@gmail.com>
2020-04-20 16:58:03 +0900

    Authenticated bind

--- index.js
+++ index.js
@@ -8,21 +8,19 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
+const expressSesstion = require('express-session')
+app.use(expressSesstion({
+  secret: 'keyboard cat'
+}))
+
+// authenticate get in posts/store and posts/new 
+const authMiddleware = require('./middleware/authMiddleware')
+// Autenticated bind in register and login
+const redirectIfAuthenticatedMiddleware = require('./middleware/redirectIfAuthenticatedMiddleware')
+
+// file involve
 const fileUpload = require('express-fileupload')
-
-const homeController = require('./controllers/home.js')
-const storePostController = require('./controllers/storePost.js')
-const getPostController = require('./controllers/getPost.js')
-const newPostController = require('./controllers/newPost.js')
-
-const newUserController = require('./controllers/newUser')
-const storeUserController = require('./controllers/storeUser')
-
-const loginController = require('./controllers/login')
-const loginUserController = require('./controllers/loginUser')
-
-// const validateMiddleware = require("./middleware/validationMiddleware")
-// app.use('/posts/store',validateMiddleware)
+app.use(fileUpload())
 
 app.set('view engine','ejs')
 app.use(express.static('public'))
@@ -32,16 +30,36 @@ app.listen(4000, ()=>{
   console.log('App listening on port 4000')
 })
 
-app.use(fileUpload())
+// post section involve
+const homeController = require('./controllers/homePage.js')
+const storePostController = require('./controllers/storePost.js')
+const getPostController = require('./controllers/getPost.js')
+const newPostController = require('./controllers/newPost.js')
 
 app.get('/',homeController)
 app.get('/post/:id',getPostController)
-app.post('/posts/store',storePostController)
-app.get('/posts/new',newPostController)
+app.post('/posts/store',authMiddleware, storePostController)
+app.get('/posts/new',authMiddleware, newPostController)
 
-app.get('/auth/register', newUserController)
-app.post('/users/register', storeUserController)
+// create user involve
+const newUserController = require('./controllers/newUser')
+const storeUserController = require('./controllers/storeUser')
 
-app.get('/auth/login', loginController)
-app.post('/users/login',loginUserController)
+app.get('/auth/register',redirectIfAuthenticatedMiddleware, newUserController)
+app.post('/users/register',redirectIfAuthenticatedMiddleware, storeUserController)
+
+// login user involve
+const loginController = require('./controllers/login')
+const loginUserController = require('./controllers/loginUser')
+
+app.get('/auth/login',redirectIfAuthenticatedMiddleware, loginController)
+app.post('/users/login',redirectIfAuthenticatedMiddleware, loginUserController)
+
+// const validateMiddleware = require("./middleware/validationMiddleware")
+// app.use('/posts/store',validateMiddleware)
+
+
+
+
+
 
