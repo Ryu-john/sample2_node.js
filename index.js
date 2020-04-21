bec65913c55504027e8aab3593d8884ab76cf150
john <ryu.surface@gmail.com>
2020-04-21 16:48:42 +0900

    chapter11 finnished

--- index.js
+++ index.js
@@ -55,11 +55,20 @@ const loginUserController = require('./controllers/loginUser')
 app.get('/auth/login',redirectIfAuthenticatedMiddleware, loginController)
 app.post('/users/login',redirectIfAuthenticatedMiddleware, loginUserController)
 
+//logout user involve
+const logoutController = require('./controllers/logout')
+
+app.get('/auth/logout', logoutController)
+
 // const validateMiddleware = require("./middleware/validationMiddleware")
 // app.use('/posts/store',validateMiddleware)
 
+// after loggin hide navbar
+global.loggedIn = null
+app.use("*", (req, res, next) => {
+  loggedIn = req.session.userId,
+  next()
+})
 
-
-
-
-
+// 404 page
+app.use((req,res) => res.render('notfound'))
